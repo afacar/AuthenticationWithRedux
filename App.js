ccab918c095b794ca52bb15cee4af2836b3ff006
unknown <acarahmetfaruk@gmail.com>
2018-08-23 23:21:30 +0300

    First Redux App

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import reducers from './src/reducers';
+import firebase from 'firebase';
+import LoginForm from './src/components/LoginForm';
+
+export default class App extends React.Component {
+  componentWillMount(){
+    var config = {
+      apiKey: "AIzaSyAglE9W7cvlesX7qWjh9bbXOE4GrnF_SXo",
+      authDomain: "employer-729b9.firebaseapp.com",
+      databaseURL: "https://employer-729b9.firebaseio.com",
+      projectId: "employer-729b9",
+      storageBucket: "employer-729b9.appspot.com",
+      messagingSenderId: "131600478375"
+    };
+    firebase.initializeApp(config);
+  }
+  
+  render() {
+    return (
+      <Provider store={ createStore(reducers, {}, applyMiddleware(ReduxThunk)) } >
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ src/actions/index.js
@@ -0,0 +1,55 @@
+import firebase from 'firebase';
+import { 
+    EMAIL_CHANGED, 
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_FAIL, 
+    LOGIN_USER} from './types';
+
+export const emailChanged = (text) => {
+    return {
+        type: EMAIL_CHANGED,
+        payload: text
+    };
+};
+
+export const passwordChanged = (text) => {
+    return {
+        type: PASSWORD_CHANGED,
+        payload: text
+    }
+};
+
+export const loginUser = ({ email, password }) => {
+    return (dispatch) => {
+        dispatch({ type: LOGIN_USER });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then( user => loginUserSuccess(dispatch, user))
+        .catch( (error) => {
+            // a Firebase GOTCHA!!!
+            // This error is important because,
+            // if there is some error in AuthReducer.js,
+            // firebase considers this error as its own
+            // and falls in catch statement
+            console.log(error);
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then( user => loginUserSuccess(dispatch, user))
+            .catch((error) => {
+                console.log('CreateUser Error: ', error);
+                loginUserFail(dispatch)
+            });
+        });
+    };
+};
+
+const loginUserFail = (dispatch) => {
+    dispatch({ type: LOGIN_USER_FAIL });
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
